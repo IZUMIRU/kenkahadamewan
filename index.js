54370877d6d4cf61a2a6c0482ceb4f06f3582fdb
IZUMIRU <izumiruryousuke@gmail.com>
2019-08-03 23:59:03 +0900

    fix: GCNLを叩く

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ function main() {
   const config = {
     channelAccessToken: process.env.LINE_ACCESS_TOKEN,
     channelSecret: process.env.LINE_CHANNEL_SECRET,
-    apiKey: process.env.GCNL_API_KEY
   };
   server.listen(process.env.PORT || 3000);
   const bot = new line.Client(config);
@@ -21,10 +20,10 @@ function main() {
 
     req.body.events.forEach((event) => {
       if (event.type == 'message' && event.message.type == 'text'){
-        if (event.message.text == 'こんにちは'){
+        if(analyzeSentiment(event.message.text) === 'negative') {
           events_processed.push(bot.replyMessage(event.replyToken, {
             type: 'text',
-            text: 'あいうえお'
+            text: sentiment
           }));
         }
       }
@@ -36,4 +35,35 @@ function main() {
       }
     );
   });
+}
+
+/**
+ * GoogleCloudNaturalLanguageAPIを叩いて、
+ * LINEに送信されたテキストがpositiveかnegativeか判別する
+ *
+ * @param string message
+ * @return string sentiment
+ */
+function analyzeSentiment(message) {
+  const apiKey = process.env.GCNL_API_KEY;
+  const url    = 'https://language.googleapis.com/v1/documents:analyzeSentiment?key=' + apiKey;
+  const data   = {
+    'document' : {
+      'type'     : 'PLAIN_TEXT',
+      'language' : 'ja',
+      'content'  : message
+    },
+    'encodingType': 'UTF8'
+  };
+  const params = {
+    'contentType' : 'application/json',
+    'method'      : 'post',
+    'payload'     : JSON.stringify(data)
+  };
+
+  const result    = UrlFetchApp.fetch(url, params);
+  const score     = JSON.parse(result)['documentSentiment']['score'];
+  const sentiment = score >= 0 ? 'positive' : 'negative';
+
+  return sentiment;
 }
