2348783aa04b65ef2b4285ff1daf7d1e8aa2d16c
IZUMIRU <izumiruryousuke@gmail.com>
2019-08-03 23:38:15 +0900

    fix: configをmain()の中に持ってくる

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-const server = require('express')();
-const line   = require('@line/bot-sdk');
-const config = {
-    channelAccessToken: process.env.LINE_ACCESS_TOKEN,
-    channelSecret: process.env.LINE_CHANNEL_SECRE,
-    apiKey: process.env.GCNL_API_KEY
-};
-server.listen(process.env.PORT || 3000);
-const bot = new line.Client(config);
-
 main();
 
 /**
  * メイン処理
  */
 function main() {
-    server.post('/bot/webhook', line.middleware(config), (req, res, next) => {
-        res.sendStatus(200);
+  const server = require('express')();
+  const line   = require('@line/bot-sdk');
+  const config = {
+    channelAccessToken: process.env.LINE_ACCESS_TOKEN,
+    channelSecret: process.env.LINE_CHANNEL_SECRE,
+    apiKey: process.env.GCNL_API_KEY
+  };
+  server.listen(process.env.PORT || 3000);
+  const bot = new line.Client(config);
 
-        let events_processed = [];
+  server.post('/bot/webhook', line.middleware(config), (req, res, next) => {
+    res.sendStatus(200);
 
-        req.body.events.forEach((event) => {
-            if (event.type == 'message' && event.message.type == 'text'){
-                if (event.message.text == 'こんにちは'){
-                    events_processed.push(bot.replyMessage(event.replyToken, {
-                        type: 'text',
-                        text: 'あいうえお'
-                    }));
-                }
-            }
-        });
+    let events_processed = [];
 
-        Promise.all(events_processed).then(
-            (response) => {
-                console.log(`${response.length} event(s) processed.`);
-            }
-        );
+    req.body.events.forEach((event) => {
+      if (event.type == 'message' && event.message.type == 'text'){
+        if (event.message.text == 'こんにちは'){
+          events_processed.push(bot.replyMessage(event.replyToken, {
+            type: 'text',
+            text: 'あいうえお'
+          }));
+        }
+      }
     });
+
+    Promise.all(events_processed).then(
+      (response) => {
+        console.log(`${response.length} event(s) processed.`);
+      }
+    );
+  });
 }
